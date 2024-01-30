9d7c6d79307092d89f167adfc1871512beab0fdb
patelsana27 <157990962+patelsana27@users.noreply.github.com>
2024-01-30 10:41:50 +0200

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 23:19:17 2024
+
+@author: patel
+"""
+
+print("My First Python File")
+density = 1000
+Density = 900
+mass_kg = 50
+#three main types of data
+"""
+int = integar - whole number 
+float - number with decimals
+strings - letters, text, words, paragraphs
+"""
+gravity_m_2_sec = 9.8
+water_level_m_above_sea_level = 14.89
+pi = 3.14
+fruit = "banana"
+element = "Potassium"
+parasite = "Plasmodium"
